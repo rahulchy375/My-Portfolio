2511b26f37da2010e9a2eef9245da0d1a50f0c0c
Rahul <rahulchy7.8ui@gmail.com>
2024-11-09 22:21:23 +0600

    again

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -119,21 +119,30 @@ const ContactSection = () => {
         <div className="dark:text-white">
           <div className="">
             <div className="sm:flex items-center justify-between">
-              <h2 ref={title} className="text-[25px] sm:text-[30px] lg:text-[40px] font-bold">
+              <h2
+                ref={title}
+                className="text-[25px] sm:text-[30px] lg:text-[40px] font-bold"
+              >
                 Contact{" "}
                 <span className="text-[tomato] dark:text-[#0ef]">Me</span>
               </h2>
               <div className="w-full sm:w-[70%] md:w-[75%] xl:w-[80%] h-[3px] bg-[tomato] dark:bg-[#0ef] rounded-[10px] order-2 sm:order-1"></div>
             </div>
 
-            <p ref={para} className="mt-[15px] text-[#8A8FB9] md:text-[18px] lg:text-[20px] dark:text-white">
+            <p
+              ref={para}
+              className="mt-[15px] text-[#8A8FB9] md:text-[18px] lg:text-[20px] dark:text-white"
+            >
               If you need any kind of website so just <br />
               fill the form and send the mail for contacting with me.
             </p>
           </div>
 
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
-            <div ref={inputs} className="my-[30px] order-2 w-full sm:order-1 sm:w-[48%] xl:w-[49%]">
+            <div
+              ref={inputs}
+              className="my-[30px] order-2 w-full sm:order-1 sm:w-[48%] xl:w-[49%]"
+            >
               <div className="md:flex gap-[10px] lg:justify-between">
                 <ServiceInput
                   title="Name"
@@ -193,17 +202,20 @@ const ContactSection = () => {
                 </div>
               ) : (
                 <div className="" ref={button}>
-                <button
-                  className="px-[20px] py-[7px] rounded-[20px] text-white dark:text-[#151875] bg-[tomato] dark:bg-[#0ef] mt-[20px] hover:scale-[1.05] duration-300"
-                  onClick={handleSendMessage}
-                >
-                  Send Message
-                </button>
+                  <button
+                    className="px-[20px] py-[7px] rounded-[20px] text-white dark:text-[#151875] bg-[tomato] dark:bg-[#0ef] mt-[20px] hover:scale-[1.05] duration-300"
+                    onClick={handleSendMessage}
+                  >
+                    Send Message
+                  </button>
                 </div>
               )}
             </div>
 
-            <div className="order-1 sm:order-2 mt-[30px] sm:w-[48%] xl:w-[49%]" ref={map}>
+            <div
+              className="order-1 sm:order-2 mt-[30px] sm:w-[48%] xl:w-[49%]"
+              ref={map}
+            >
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d236153.21826365963!2d91.65422151951107!3d22.357629611510667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8a64095dfd3%3A0x5015cc5bcb6905d9!2sChattogram!5e0!3m2!1sen!2sbd!4v1730451617510!5m2!1sen!2sbd"
                 className="border-none w-full h-[200px] sm:h-[300px] md:h-[350px] xl:h-[400px]"
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,13 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the Firebase SDKs
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // Ensure the value format is "my-portfolio-7a24f.appspot.com"
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
@@ -16,4 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default firebaseConfig
+const database = getDatabase(app);
+
+export { app, database };
